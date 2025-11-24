a3841d5ca0d9fe625ba8eba94879a80919728589
Keerthana Adamana <kadamana012@gmail.com>
2025-11-24 23:57:09 +0000

    stock history and daily files are added

--- models/stock_internal/stock_daily.sql
+++ models/stock_internal/stock_daily.sql
@@ -1,3 +1,10 @@
+/*{{ config(transient = false)}}*/
+with raw_stock_data as (
+    select * from EDW_DEV.RAW.nse_stock_daily_data
+)
+select 
+        SYMBOL as stock_name, 
+        --xyz,
+        SERIES,OPEN,HIGH,LOW,CLOSE,LAST,PREVCLOSE,TOTTRDQTY as total_quantity_traded,TOTTRDVAL as total_trade_val,TIMESTAMP as Date,TOTALTRADES,ISIN
 
-{{config(materialized = 'table')}}
-select * from EDW_DEV.RAW.nse_stock_daily_data
+from raw_stock_data
