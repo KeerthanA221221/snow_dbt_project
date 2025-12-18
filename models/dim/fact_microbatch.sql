d8343fcc48172ec2bf9fb3ef40dd22422bb6c9ea
Keerthana Adamana <kadamana012@gmail.com>
2025-12-18 17:10:37 +0000

    dbt seed

--- models/dim/fact_microbatch.sql
+++ models/dim/fact_microbatch.sql
@@ -2,10 +2,12 @@
     config(
         materialized = 'incremental',
         incremental_strategy = 'microbatch',
-        event_time = 'ORDER_TS',
+        event_time = 'last_modified_date',
         begin= '2025-12-14',
         batch_size = 'day',
-        unique_key = 'order_id'
+        unique_key = 'order_id', 
+        post_hook = "update EDW_DEV.RAW.flatten_orders set customer_id = 'pl7q524' where order_id = 'O62e427b5'",
+        pre_hook = "update EDW_DEV.DBT_KADAMANA.fact_microbatch set customer_id = 'pl7q524' where order_id = 'O62e427b5'"
     )
 }}
 
